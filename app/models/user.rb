de06501fb5e08c576d90cca63f18afd3707657ff
uzura <wakabayashita@firstserver.co.jp>
2015-12-22 13:28:54 +0900

    Finish log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   before_save {
@@ -7,7 +9,7 @@ class User < ActiveRecord::Base
 
   validates :name,
   presence: true,
-  length: { maximum: 50  }
+  length: { maximum: 50 }
 
   validates :email,
   presence: true,
@@ -20,4 +22,32 @@ class User < ActiveRecord::Base
   length: { minimum: 6 }
 
   has_secure_password
+
+  # 与えられた文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続的セッションで使用するユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーログインを破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,5 +22,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
     assert_template 'users/show'
     assert flash.any?
+    assert is_logged_in?
   end
 end
