b686c7e13a6444c092ec5da1e6b91308e4d67740
uzura <wakabayashita@firstserver.co.jp>
2016-01-07 11:32:16 +0900

    Change application name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'safe-chamber-7646.herokuapp.com'
+  host = 'ese-twitter2.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
